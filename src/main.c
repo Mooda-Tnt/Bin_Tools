aad78b3ea1390e2cad8099c794a22e8dcef5d765
Mooda-Tnt <49732230+Mooda-Tnt@users.noreply.github.com>
2026-09-05 23:11:51 +0300

    Add main.c

---
+++ src/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum {UNSUPPORTED = -1, ELF, PE} File_Format;
+
+size_t get_binary_size(FILE * binary_file)
+{
+	// TODO:
+	// Implement get_binary_size().
+	return 4;
+}
+
+File_Format identify_file(FILE * binary_file)
+{
+	const unsigned char elf_magic_number[4] = {'\x7f', '\x45', '\x4c', '\x46'};
+	
+	unsigned char magic_number[4];
+	fread(magic_number, sizeof(*magic_number), sizeof(magic_number) / sizeof(*magic_number), binary_file);
+
+	int result = memcmp(elf_magic_number, magic_number, 4);
+	if(result == 0)
+		return ELF;
+
+	return UNSUPPORTED;
+}
+
+int main(int argc, char **argv)
+{
+	if(argc == 1)
+    	{
+        	printf("No binary file provided!\n");
+		return EXIT_FAILURE;
+	}
+
+	char *bin_name = argv[1];
+	
+	FILE *bin = fopen(bin_name, "rb");
+	if(!bin)
+	{
+		printf("Binary file does not exist!\n");
+		return EXIT_FAILURE;
+	}
+
+	File_Format ff = identify_file(bin);
+        if(ff)
+        {
+                printf("Unsupported binary format!\n");
+                return EXIT_FAILURE;
+        }
+
+	printf("File Format: ELF\n");
+
+	fclose(bin);
+	return 0;
+}
