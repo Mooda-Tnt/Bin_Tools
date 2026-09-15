411aafc07dc97590b357629dc3c6360b4c735de3
Mohamad Alhamwi <49732230+Mooda-Tnt@users.noreply.github.com>
2026-09-15 23:22:28 +0300

    Update main.c
    Delete extra tabs.

--- src/main.c
+++ src/main.c
@@ -16,9 +16,9 @@ long get_binary_size(FILE * bin)
 	long size = ftell(bin);
 	if(size == -1L)
 	{
-                printf("An error occured while calculating the size of the given binary.\n");
-                return -1L;
-        }
+		printf("An error occured while calculating the size of the given binary.\n");
+        return -1L;
+	}
 
 	rewind(bin);
 	return size;
@@ -41,8 +41,8 @@ File_Format identify_file(FILE * binary_file)
 int main(int argc, char **argv)
 {
 	if(argc == 1)
-    	{
-        	printf("No binary file provided!\n");
+    {
+    	printf("No binary file provided!\n");
 		return EXIT_FAILURE;
 	}
 
@@ -57,23 +57,23 @@ int main(int argc, char **argv)
 	
 	long bin_size = get_binary_size(bin);
 	if(bin_size == -1L)
-    		return EXIT_FAILURE;
+    	return EXIT_FAILURE;
 
-        if(bin_size < 4)
-        {
-                printf("Malformed binary file\n");
-                return EXIT_FAILURE;
-        }
+	if(bin_size < 4)
+	{
+		printf("Malformed binary file\n");
+		return EXIT_FAILURE;
+	}
 
 	// Debugging line.
 	printf("%ld\n", bin_size);
 
 	File_Format ff = identify_file(bin);
-        if(ff)
-        {
-                printf("Unsupported binary format!\n");
-                return EXIT_FAILURE;
-        }
+	if(ff)
+	{
+			printf("Unsupported binary format!\n");
+			return EXIT_FAILURE;
+	}
 
 	printf("File Format: ELF\n");
 
