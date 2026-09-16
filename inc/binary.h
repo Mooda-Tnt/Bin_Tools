1fc2d2ea6415cbcf80c3618372c222638bc59eab
Mohamad Alhamwi <49732230+Mooda-Tnt@users.noreply.github.com>
2026-09-16 22:53:28 +0300

    Create binary.h

---
+++ inc/binary.h
@@ -0,0 +1,18 @@
+#ifndef BINARY_H
+#define BINARY_H
+
+#include <stdio.h>
+
+#include "errors.h"
+
+#define MAGIC_NUMBER_SIZE 4
+#define DOS_MAGIC_NUMBER_SIZE 2
+#define E_LFANEW_OFFSET 0X3C
+
+typedef enum {UNSUPPORTED = -1, ELF, PE} File_Format;
+
+Error_Code open_binary_r(char *bin_name, FILE **bin);
+Error_Code get_binary_size(FILE *bin, long *size);
+Error_Code identify_binary(FILE *bin, File_Format *file_format);
+
+#endif
