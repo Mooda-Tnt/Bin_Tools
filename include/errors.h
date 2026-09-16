31ffb74965094444c31384b1f73b20d70c3ac058
Mooda-Tnt <49732230+Mooda-Tnt@users.noreply.github.com>
2026-09-16 16:42:25 +0300

    Add new module.

---
+++ include/errors.h
@@ -0,0 +1,16 @@
+#ifndef ERRORS_H
+#define ERRORS_H
+
+typedef enum
+{
+    ERR_NONE = 0,
+
+    ERR_FILE_NOT_FOUND = 1,
+    ERR_FILE_READ_FAILED = 2,
+    ERR_FILE_WRITE_FAILED = 3,
+    ERR_FILE_ACCESS_FAILED = 4,
+    ERR_FILE_SIZE_CALCULATION_FAILED = 5
+
+} Error_Code;
+
+#endif
