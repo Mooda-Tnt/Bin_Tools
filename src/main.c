e71cd8366dcd5006dac4ebd82dae3033bbfd3ea6
Mohamad Alhamwi <49732230+Mooda-Tnt@users.noreply.github.com>
2026-09-16 14:36:20 +0300

    Update main.c
    Add missing micros.

--- src/main.c
+++ src/main.c
@@ -3,6 +3,10 @@
 #include <stdint.h>
 #include <string.h>
 
+#define MAGIC_NUMBER_SIZE 4
+#define DOS_MAGIC_NUMBER_SIZE 2
+#define E_LFANEW_OFFSET 0X3C
+
 typedef enum
 {
     ERR_NONE = 0,
@@ -36,16 +40,16 @@ Error_Code get_binary_size(FILE *bin, long *size)
 Error_Code identify_file(FILE *bin, File_Format *file_format)
 {
 	const unsigned char Elf_Magic_Number[MAGIC_NUMBER_SIZE] = {'\x7f', '\x45', '\x4c', '\x46'};
-    const unsigned char Dos_Magic_Number[DOS_MAGIC_NUMBER_SIZE] = {'\x4d', '\x5a'};
-    const unsigned char Pe_Magic_Number[MAGIC_NUMBER_SIZE] = {'\x50', '\x45', '\x00', '\x00'};
+	const unsigned char Dos_Magic_Number[DOS_MAGIC_NUMBER_SIZE] = {'\x4d', '\x5a'};
+	const unsigned char Pe_Magic_Number[MAGIC_NUMBER_SIZE] = {'\x50', '\x45', '\x00', '\x00'};
 
     unsigned char magic_number[MAGIC_NUMBER_SIZE];
     if(fread(magic_number, sizeof(*magic_number), MAGIC_NUMBER_SIZE, bin) != MAGIC_NUMBER_SIZE)
 		return ERR_FILE_READ_FAILED;
 
-    if(!memcmp(Elf_Magic_Number, magic_number, MAGIC_NUMBER_SIZE))
-    	*file_format = ELF;
-
+	if(!memcmp(Elf_Magic_Number, magic_number, MAGIC_NUMBER_SIZE))
+		*file_format = ELF;
+	
 	else if(!memcmp(Dos_Magic_Number, magic_number, DOS_MAGIC_NUMBER_SIZE))
 	{
 		uint32_t e_lfanew;
@@ -72,7 +76,7 @@ Error_Code identify_file(FILE *bin, File_Format *file_format)
 			*file_format = PE;
 		else
 			*file_format = UNSUPPORTED;
-    }
+	}
 
 	else
 		*file_format = UNSUPPORTED;
