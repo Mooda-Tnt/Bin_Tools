1c872aaa9af8436013ff237fbb8eba4c64c78b99
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2026-09-17 10:20:59 +0300

    Update ...

--- src/errors.c
+++ src/errors.c
@@ -9,20 +9,26 @@ const char *get_error_message(Error_Code err_code)
                 case ERR_NONE:
                         error_message = "No error encountered";
                         break;
-                case ERR_FILE_NOT_FOUND:
+                case ERR_BIN_NOT_FOUND:
                         error_message = "No such binary file";
                         break;
-                case ERR_FILE_READ_FAILED:
+                case ERR_BIN_READ_FAILED:
                         error_message = "Failed to read binary file";
                         break;
-                case ERR_FILE_WRITE_FAILED:
+                case ERR_BIN_WRITE_FAILED:
                         error_message = "Failed to write to binary file";
                         break;
-                case ERR_FILE_ACCESS_FAILED:
+                case ERR_BIN_ACCESS_FAILED:
                         error_message = "Failed to traverse binary file";
                         break;
-                case ERR_FILE_SIZE_CALCULATION_FAILED:
+                case ERR_BIN_SIZE_CALCULATION_FAILED:
                         error_message = "Failed to get size of binary file";
+                        break;
+                case ERR_BIN_CLOSING_FAILED:
+                        error_message = "Failed to close binary file";
+                        break;
+                case ERR_BIN_MALFORMED:
+                        error_message = "Malformed binary file";
                         break;
                 default:
                         error_message = "Unknown error";
